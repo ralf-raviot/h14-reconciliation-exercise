36784ae49dc15f234db8b8d7d1f9c996d22958d1
ralf <ralf.de.saint.anthost@gmail.com>
2026-01-25 19:39:48 +0100

    Introduce consolidation status computation in ConsolidatedPosition domain model
    Signed-off-by: ralf <ralf.de.saint.anthost@gmail.com>

--- backend/src/main/java/it/h14/backend/domain/ConsolidatedPosition.java
+++ backend/src/main/java/it/h14/backend/domain/ConsolidatedPosition.java
@@ -3,14 +3,33 @@ package it.h14.backend.domain;
 import jakarta.annotation.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 
 public record ConsolidatedPosition(Security security,
                                    @Nullable Position internalPosition,
                                    List<Position> bankPositions) {
 
+    public ConsolidationStatus computeConsolidationStatus() {
+        if (internalPosition == null) {
+            return ConsolidationStatus.UNKNOWN_POSITION;
+        }
+        if (bankPositions.isEmpty()) {
+            return ConsolidationStatus.MISSING_BANK_POSITION;
+        }
+        if (!Objects.equals(internalPosition.quantity(), getTotalBankQuantity())) {
+            return ConsolidationStatus.NOT_CONSOLIDATED;
+        }
+        return ConsolidationStatus.CONSOLIDATED;
+    }
+
+    private Double getTotalBankQuantity() {
+        return bankPositions.stream().mapToDouble(Position::quantity).sum();
+    }
+
     public enum ConsolidationStatus {
         CONSOLIDATED,
         NOT_CONSOLIDATED,
-        UNKNOWN_POSITION
+        UNKNOWN_POSITION,
+        MISSING_BANK_POSITION
     }
 }
